e1ee5501d4de38e977e98cc81e270b339b02987b
Tejas Singh <tejassingh649@rediffmail.com>
2025-08-07 11:49:45 +0000

    extras: Set default game props [SQUASHED]
    - [tejas101k] - Originally set default game props.
    - [donjohanliebert] - Moved them to product.prop for better structure.
    
    Change-Id: I91ce80320642aa35e0adf47aa90ee5545f7d9d29
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- evolution.mk
+++ evolution.mk
@@ -132,6 +132,8 @@ PRODUCT_PACKAGES += \
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 
+TARGET_PRODUCT_PROP += vendor/extras/gameprops/gameprops.prop
+
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/extras/prebuilt/product/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
     $(call find-copy-subdir-files,*,vendor/extras/prebuilt/product/media/audio/ui,$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui)
