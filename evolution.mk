6a09b28c98c7c9df084b6793aa796810a55a8edd
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-01-02 09:55:31 +0900

    extras: stopship vendor fonts
    this change reduces typeface's memory usage
    
    Change-Id: I0aa30073f12fb5d7094c42a1d0cd7865ae87089f
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- evolution.mk
+++ evolution.mk
@@ -19,54 +19,9 @@ PRODUCT_PACKAGES += \
 # Fonts
 PRODUCT_PACKAGES += \
     fonts_customization.xml \
-    FontAccuratistOverlay \
-    FontAclonicaOverlay \
-    FontAmaranteOverlay \
-    FontBariolOverlay \
-    FontCagliostroOverlay \
-    FontCoconOverlay \
-    FontComfortaaOverlay \
-    FontComicSansOverlay \
-    FontCookieRunOverlay \
-    FontCooljazzOverlay \
-    FontCoolstoryOverlay \
-    FontExotwoOverlay \
-    FontEvoSansOverlay \
-    FontEvolveSansOverlay \
-    FontFifa2018Overlay \
-    FontGrandHotelOverlay \
-    FontGeneralSansOverlay \
     FontGoogleSansFlexOverlay \
-    FontHarmonySansOverlay \
-    FontIBMPlexSansOverlay \
     FontLatoOverlay \
-    FontLinotteOverlay \
-    FontNokiaPureOverlay \
-    FontNothingDotHeadlineOverlay \
-    FontNothingDotOverlay \
-    FontNunitoOverlay \
-    FontOneUISansOverlay \
-    FontOneplusSansOverlay \
-    FontOneplusSlateOverlay \
-    FontPoppinsSourceOverlay \
-    FontOswaldOverlay \
-    FontPlayOverlay \
-    FontQuandoOverlay \
-    FontRedressedOverlay \
-    FontReemKufiOverlay \
-    FontRobotoCondensedOverlay \
-    FontRookeryOverlay \
-    FontRubikOverlay \
-    FontSamsungOneOverlay \
-    FontSanFranciscoDisplayProSourceOverlay \
-    FontSansSerifOverlay \
-    FontSansSerifProOverlay \
-    FontSonySketchOverlay \
-    FontSpaceGroteskOverlay \
-    FontStoropiaOverlay \
-    FontSurferOverlay \
-    FontUbuntuOverlay \
-    FontVolteOverlay
+    FontRubikOverlay
 
 # Icon Shapes
 PRODUCT_PACKAGES += \
